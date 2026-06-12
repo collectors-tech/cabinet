24d6bce74e92cb67e12c29807740810052968bee
wildone <max.barrass@gmail.com>
2026-06-13 01:41:40 +1000

    #1093 test(helpcenter): cover article search and routing

--- ui.web/src/routes/_authenticated/help-center/index.tsx
+++ ui.web/src/routes/_authenticated/help-center/index.tsx
@@ -1,6 +1,14 @@
+import z from 'zod'
 import { createFileRoute } from '@tanstack/react-router'
 import { HelpCenter } from '@/features/help-center'
 
+const helpCenterSearchSchema = z.object({
+  article: z.string().optional().catch(''),
+  category: z.string().optional().catch(''),
+  q: z.string().optional().catch(''),
+})
+
 export const Route = createFileRoute('/_authenticated/help-center/')({
+  validateSearch: helpCenterSearchSchema,
   component: HelpCenter,
 })
